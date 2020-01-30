d014d0b3536d9695b4dad0523a57fbf2bafb0003
Alba Vega <avegac01@informatica.iesvalledeljerteplasencia.es>
2020-01-30 09:44:41 +0100

    Modificación clase Casilla
    Añadidos dos atributos nuevos a la clase Casilla para recoger las coordenadas donde empieza cada casilla del tablero.

--- src/utiles/Casilla.java
+++ src/utiles/Casilla.java
@@ -10,17 +10,25 @@ package utiles;
 public class Casilla {
 	private String id;
 	private int state;
-	
+	private int posX;
+	private int posY;
+		
 	/**
 	 * Constructor parametrizado
-	 * @param id identificador de la casilla
-	 * @param state estado de la casilla (ocupada o no)
+	 * @param id	Identificador de la casilla
+	 * @param posX	Coordenada X de inicio de la casilla
+	 * @param posY	Coordenada Y de inicio de la casilla
+	 * @param state	Estado de la casilla (ocupada o no). Si está vacía valdrá -1.
 	 */
-	public Casilla(String id, int state) {
+	public Casilla(String id, int posX, int posY, int state) {
 		super();
 		this.id=id;
+		this.posX=posX;
+		this.posY=posY;
 		this.state=state;
 	}
+	
+	/**GETTERS Y SETTERS**/
 	
 	public String getId() {
 		return id;
@@ -28,6 +36,22 @@ public class Casilla {
 	
 	public void setId(String id) {
 		this.id = id;
+	}
+	
+	public int getPosX() {
+		return posX;
+	}
+	
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	
+	public int getPosY() {
+		return posY;
+	}
+	
+	public void setPosY(int posY) {
+		this.posY = posY;
 	}
 	
 	public int getState() {
